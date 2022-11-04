5fcd423ff59a03eeac4aacad57e59b6a7c796471
pierluigimarchioro <marchioropierluigi@gmail.com>
2022-11-04 09:13:53 +0100

    feat(sudoku): added some docs; added empty_cell_value param to SudokuGrid __init__(); added is_full() method to SudokuGrid

--- src/sudoku.py
+++ src/sudoku.py
@@ -11,7 +11,11 @@ class SudokuGrid:
     grid: np.ndarray
 
     def __init__(self, starting_grid: np.ndarray = None):
-        """"""
+        """
+        :param starting_grid:
+        """
+
+        assert starting_grid.shape[0] == 9 and starting_grid.shape[1] == 9, "Provided sudoku grid is not 9x9"
 
         self.grid = np.full((9, 9), EMPTY_CELL_VALUE) if starting_grid is None else starting_grid
 
@@ -32,9 +36,13 @@ class SudokuGrid:
     def get_square(self, starting_row: int, starting_col: int) -> np.ndarray:
         return self.grid[starting_row:starting_row+3, starting_col:starting_col+3]
 
+    def is_full(self) -> bool:
+        # Full when there are no more empty cells
+        return EMPTY_CELL_VALUE not in self.grid
+
 
 @dataclass(frozen=True)
-class SudokuCell:
+class SudokuCell:  # TODO cell coordinates?
     row: int
     col: int
 
