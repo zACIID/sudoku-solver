8555c9e7c9f588aeb30a23637339d73aafae6250
pierluigimarchioro <marchioropierluigi@gmail.com>
2022-11-04 09:06:38 +0100

    fix+feat(constraints): fixed check_no_repetitions method (now works with partial rows/cols/squares); added functions to check if sudoku (even partial) and solutions are correct

--- src/constraints.py
+++ src/constraints.py
@@ -1,23 +1,68 @@
 import numpy as np
 
+import sudoku as sud
+
+
+def is_solution_correct(solution: sud.SudokuGrid) -> bool:
+    """
+    Checks if a sudoku solution is correct, which means that the provided
+    sudoku grid must be both full and valid.
+
+    :param solution: complete sudoku grid
+    :return: True if the solution is correct
+    """
+
+    return solution.is_full() and is_grid_valid(solution)
+
+
+def is_grid_valid(grid: sud.SudokuGrid) -> bool:
+    """
+    Checks that each column, row and 3x3 square don't contain duplicates
+    of numbers from 1 to 9. Returns True if no repetitions.
+
+    :param grid: sudoku grid to check
+    :return: True if no repetitions found
+    """
+
+    valid = True
+
+    # Check Rows and Columns
+    for i in range(0, 8+1):
+        valid &= check_no_repetitions(grid.get_row(i))
+        valid &= check_no_repetitions(grid.get_column(i))
+
+        if not valid:
+            return False
+
+    # Check all the nine 3x3 squares
+    for i in range(0, 8+1, 3):
+        for j in range(0, 8+1, 3):
+            if not check_no_repetitions(grid.get_square(
+                starting_row=i, starting_col=j
+            )):
+                return False
+
+    return True
+
 
 def check_no_repetitions(array: np.ndarray) -> bool:
     """
-    Checks the provided array for no repetitions.
-    The array should have cardinality equal to 9, and should contain only numbers from 1 to 9.
-    Returns True if all the numbers are present exactly once.
+    Checks the provided array for no repetitions in the numbers between 1 and 9.
+    This means that both an incomplete or complete sudoku column/row/box can be valid,
+    iff there are no repetitions except for the values used for empty cells, which
+    certainly do not belong in the set of integers from 1 to 9.
 
-    :param array: array with exactly 9 elements
-    :return: True if the array contains all the numbers from 1 to 9 exactly once
+    :param array: array with exactly 9 elements that represents either
+        a sudoku column, row or 3x3 box
+    :return: True if any number between 1 and 9 is contained exactly once
     """
 
-    # This function checks collections of exactly 9 elements,
-    # which could be 3x3 squares, 1x9 rows or 1x9 columns
-    # Cardinality MUST be 9
     assert array.size == 9, "The provided array has to be exactly of length 9"
 
-    # Since the array is of size 9, np.all evaluates to True iff
-    # each number 1 to 9 is contained exactly once
-    # TODO this works to check if a complete sudoku grid is valid, but it doesn't work
-    #   for partial rows/columns/squares
-    return np.all(np.isin(array, [1, 2, 3, 4, 5, 6, 7, 8, 9]))
+    # Important is to check that duplicate elements are not 1 to 9 numbers
+    # Remember, in fact, that there could also be duplicate filler
+    #   values for empty cells
+    unique, counts = np.unique(array, return_counts=True)
+    duplicates = unique[counts > 1]
+
+    return not np.any(np.isin(duplicates, range(1, 9+1)))
