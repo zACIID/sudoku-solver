076a960da0012b9fe2f8543eadbfa763b5752a60
pierluigimarchioro <marchioropierluigi@gmail.com>
2022-11-12 19:41:27 +0100

    feat: added main.py (TODO)

---
+++ src/main.py
@@ -0,0 +1,4 @@
+# TODO add script entry point to launch the various types of solvers
+#   allow to read sudoku grid from file
+#   in general, structure the code the same way it was done with pyraptor
+#   setup.py with entry points to publish this project on PyPy??
