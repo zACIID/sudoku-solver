74fe5e4e64131f74a2e07c74fcb2cbb6c574d8cd
pierluigimarchioro <marchioropierluigi@gmail.com>
2022-11-13 22:39:42 +0100

    tests(solvers): added test for annealing solver

--- tests/test_solvers.py
+++ tests/test_solvers.py
@@ -1,8 +1,10 @@
 import pytest
 
 import src.solvers.bt_cp as bt
+import src.solvers.annealing as ann
 from src.model.sudoku_base import SudokuGrid
 from src.model.sudoku_cp import ConstraintPropagationSudokuGrid
+from src.model.sudoku_annealing import SimulatedAnnealingSudokuGrid
 from src.solvers.utils import is_solution_correct
 
 
@@ -11,4 +13,12 @@ def test_bt_solver(sudoku_grid: SudokuGrid):
     cp_grid = ConstraintPropagationSudokuGrid.from_sudoku_grid(grid=sudoku_grid)
 
     solution = bt.bt_cp_sudoku_solver(sudoku=cp_grid)
+    assert is_solution_correct(solution), "Solution should be correct"
+
+
+@pytest.mark.annealing
+def test_bt_solver(sudoku_grid: SudokuGrid):
+    ann_grid = SimulatedAnnealingSudokuGrid.from_sudoku_grid(grid=sudoku_grid)
+
+    solution = ann.simulated_annealing_solver(sudoku=ann_grid)
     assert is_solution_correct(solution), "Solution should be correct"
