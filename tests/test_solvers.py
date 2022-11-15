22dfba47062b3f51d417d3237683fe219f08f67d
pierluigimarchioro <marchioropierluigi@gmail.com>
2022-11-15 12:07:42 +0100

    tests(solvers): refactored solvers tests; now base grids are passed to the solvers

--- tests/test_solvers.py
+++ tests/test_solvers.py
@@ -1,24 +1,28 @@
 import pytest
+from loguru import logger
 
 import src.solvers.bt_cp as bt
 import src.solvers.annealing as ann
 from src.model.sudoku_base import SudokuGrid
-from src.model.sudoku_cp import ConstraintPropagationSudokuGrid
-from src.model.sudoku_annealing import SimulatedAnnealingSudokuGrid
 from src.solvers.utils import is_solution_correct
 
 
 @pytest.mark.bt_cp
 def test_bt_solver(sudoku_grid: SudokuGrid):
-    cp_grid = ConstraintPropagationSudokuGrid.from_sudoku_grid(grid=sudoku_grid)
+    solution = bt.bt_cp_sudoku_solver(sudoku=sudoku_grid)
 
-    solution = bt.bt_cp_sudoku_solver(sudoku=cp_grid)
-    assert is_solution_correct(solution), "Solution should be correct"
+    assert_solution(solution)
 
 
 @pytest.mark.annealing
 def test_annealing_solver(sudoku_grid: SudokuGrid):
-    ann_grid = SimulatedAnnealingSudokuGrid.from_sudoku_grid(grid=sudoku_grid)
+    solution = ann.simulated_annealing_solver(sudoku=sudoku_grid)
 
-    solution = ann.simulated_annealing_solver(sudoku=ann_grid)
+    assert_solution(solution)
+
+
+def assert_solution(solution: SudokuGrid):
+    logger.debug("Solution to assert:\n"
+                 f"{solution}")
+
     assert is_solution_correct(solution), "Solution should be correct"
