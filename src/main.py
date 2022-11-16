e4be55e18d9772a3dbebb041a4be00cdccea67bd
pierluigimarchioro <marchioropierluigi@gmail.com>
2022-11-16 11:39:36 +0100

    chore(main): removed TODO

--- src/main.py
+++ src/main.py
@@ -1,7 +1,3 @@
-# TODO add script entry point to launch the various types of solvers
-#   allow to read sudoku grid from file
-#   in general, structure the code the same way it was done with pyraptor
-#   setup.py with entry points to publish this project on PyPy??
 from __future__ import annotations
 
 import argparse
